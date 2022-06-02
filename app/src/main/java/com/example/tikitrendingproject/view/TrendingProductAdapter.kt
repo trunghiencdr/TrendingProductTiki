65b3b5b79a5db21b62daf7c3f3b1fca6ddb54b91
trunghiencdr <tnthien.cdr@gmai.com>
2022-06-02 18:26:09 +0700

    add adapter

--- app/src/main/java/com/example/tikitrendingproject/view/TrendingProductAdapter.kt
+++ app/src/main/java/com/example/tikitrendingproject/view/TrendingProductAdapter.kt
@@ -1,4 +1,35 @@
 package com.example.tikitrendingproject.view
 
-class TrendingProductAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tikitrendingproject.databinding.ProductRowBinding
+import com.example.tikitrendingproject.model.Product
+
+class TrendingProductAdapter
+    : ListAdapter<Product, TrendingProductAdapter.MyViewHolder>(DiffUtilProduct()){
+
+    class MyViewHolder(var binding: ProductRowBinding): RecyclerView.ViewHolder(binding.root){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
+
+class DiffUtilProduct: DiffUtil.ItemCallback<Product>(){
+    override fun areItemsTheSame(oldItem: Product, newItem: Product): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun areContentsTheSame(oldItem: Product, newItem: Product): Boolean {
+        TODO("Not yet implemented")
+    }
+
 }
